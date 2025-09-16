4c359577ff0015b91193ba73cc188399fb6ccd7e
JuanDa <jlopezrodriguez95@gmail.com>
2025-09-15 23:32:37 -0500

    test(backend): ensure context test does not depend on local DB

--- backend/src/test/java/com/juanda/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/juanda/backend/BackendApplicationTests.java
@@ -1,10 +1,11 @@
 package com.juanda.backend;
 
+import com.juanda.backend.testinfra.PostgresTC;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendApplicationTests extends PostgresTC {
 
 	@Test
 	void contextLoads() {
